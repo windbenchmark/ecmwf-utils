3064be4d9a685654a0b317222dba2938e8cee363
Lawrence-Pet <peter.l.lawrence@ntnu.no>
2026-01-02 22:28:40 +0100

    Local VM changes

---
+++ run_retrieval.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Navigate to the script's directory ensuring relative paths work correctly
+cd "$(dirname "$0")"
+# Run the ECMWF retrieval command in the ecmwf-utils environment
+mamba run -n ecmwf-utils python -m src retrieval --query-path ./queries/kelmarsh-full.json --config-path ./config/config_hres.yml
