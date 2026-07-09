b6790b60ca077503cfb8bc0bbd03475a0a6f9c16
Lawrence-Pet <peter.l.lawrence@ntnu.no>
2026-07-09 14:19:46 +0200

    fixed query

--- run_retrieval.sh
+++ run_retrieval.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# WHEN RUNNING THIS DO: `setsid -f ./run_retrieval.sh >"./retrieval_<LOGNAME>_<DATE>.log" 2>&1 </dev/null
 
 # Navigate to the script's directory ensuring relative paths work correctly
 cd "$(dirname "$0")"
 # Run the ECMWF retrieval command in the ecmwf-utils environment
-mamba run -n ecmwf-utils python -m src retrieval --skip-cost --query-path ./queries/penmanshiel-full.json --config-path ./config/config_hres_10d.yml
+mamba run -n ecmwf-utils python -m src retrieval --skip-cost --query-path "queries/australia-full.json" --config-path ./config/config_hres_10d.yml
 
 
